ac03c88e04f76378d13fd1024b3e05c5d9459e6c
Juan <jaguilera@email.com>
2024-05-20 23:14:09 -0600

    php URI review

--- php/api/index.php
+++ php/api/index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo "Hello world";
+//echo $_SERVER["REQUEST_URI"];
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+$parts = explode("/", $path);
+
+$resource = $parts[2];
+
+//$id = isset($parts[3]) ? $parts[3] : null;
+$id = $parts[3] ?? null;
+
+echo $resource, ", " , $id;
