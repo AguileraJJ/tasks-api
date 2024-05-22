f4eaa3babbf27017a8e56a7714c8eab1be883efe
Juan <jaguilera@email.com>
2024-05-21 20:32:21 -0600

    index update

--- php/api/index.php
+++ php/api/index.php
@@ -10,4 +10,13 @@ $resource = $parts[2];
 //$id = isset($parts[3]) ? $parts[3] : null;
 $id = $parts[3] ?? null;
 
-echo $resource, ", " , $id;
+//echo $resource, ", " , $id;
+
+//echo $_SERVER["REQUEST_METHOD"], "\n";
+
+if ($resource != "tasks"){
+    
+    //header("{$_SERVER['SERVER_PROTOCOL']} 404 Not Found");
+    http_response_code(404);
+    exit;
+}
